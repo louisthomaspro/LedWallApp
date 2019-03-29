4ae38341cf8054bc6b5435efceb1b1d1d5363097
Louis <louisthomas.pro@gmail.com>
2019-03-29 01:09:32 +0100

    npm with sudo + ng build with pushd

--- scripts/project_install.sh
+++ scripts/project_install.sh
@@ -7,23 +7,23 @@
 msg_title "*** LedWallApp Project Initialization ***"
 
 msg_info "Recreate Public directory..."
-rm -rf ${LEDWALLAPP_HOME}
-mkdir ${LEDWALLAPP_HOME}
+rm -rf ${LEDWALLAPP_HOME} || msg_err
+mkdir ${LEDWALLAPP_HOME} || msg_err
 
 msg_info "Cloning project..."
-git clone https://github.com/louisthomaspro/LedWallApp.git ${LEDWALLAPP_HOME}
+git clone https://github.com/louisthomaspro/LedWallApp.git ${LEDWALLAPP_HOME} || msg_err
 
 msg_info "Npm install back"
-npm install --prefix ${LEDWALLAPP_HOME}/back
+sudo npm install --prefix ${LEDWALLAPP_HOME}/back || msg_err
 
 msg_info "Npm install front"
-npm install --prefix ${LEDWALLAPP_HOME}
+sudo npm install --prefix ${LEDWALLAPP_HOME}/front || msg_err
 
 msg_info "Build front"
-ng build --prod
+pushd ${LEDWALLAPP_HOME}/front && ng build --prod && popd || msg_err
 
-sh ${LEDWALLAPP_HOME}/LedWallApp/scripts/systemd_init.sh
-sh ${LEDWALLAPP_HOME}/LedWallApp/scripts/nginx_init.sh
+sh ${LEDWALLAPP_HOME}/scripts/systemd_init.sh
+sh ${LEDWALLAPP_HOME}/scripts/nginx_init.sh
 
 
 msg_finish "Project is ready !"
