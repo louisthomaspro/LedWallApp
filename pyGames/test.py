024d5c905cb3a84a1e7d2052084a00f4ce22faa4
gtazer <soufiane.simmou1@gmail.com>
2019-04-09 21:39:24 +0200

    arrows handlling

--- pyGames/test.py
+++ pyGames/test.py
@@ -8,8 +8,18 @@ from pynput import keyboard
 
 i: int = 0
 j = 0
+higth = 0
+width = 0
+
+randomVal = 0
+red: List[int] = [0xff, 0x00, 0x00]
+blue: List[int] = [0x00, 0x00, 0xff]
+wall = []
+flag = False
+
 
 def printLedWall(Byteframe):
+    
     file = open("/tmp/lwfb", "wb")
     print("file opend")
     file.write(Byteframe)
@@ -84,128 +94,74 @@ def goRight(higth, width):
     coordinates = inrange(higth, width)
     return coordinates
 
-def on_press(key):
-    try: k = key.char # single-char keys
-    except: k = key.name # other keys
-    if key == keyboard.Key.esc: return False # stop listener
-    if k in ['down', 'up', 'left', 'right']: # keys interested
-        # self.keys.append(k) # store it in global-like variable
-        print('Key pressed: ' + k)
-        return k 
-
-
-higth = 0
-width = 0
-
-randomVal = 0
-red: List[int] = [0xff, 0x00, 0x00]
-blue: List[int] = [0x00, 0x00, 0xff]
-wall = []
-flag = False
-
 while i < 160:
     wall.append(red)
     i += 1
 printLedWall(fromListToLedframe(wall))
 oldcolorpos = 0
 
-while flag == False:
-    
-    coordinates = [0,0]
-    # variable = input_char('press Z, S, D, Q to play:')
-    # print(variable)
-    lis = keyboard.Listener(on_press=on_press)
-    lis.start() # start to listen on a separate thread
-    lis.join()
-    variable = lis.getName()
-    
-    if variable == "\x1b[A":
-        
-        print(higth)
-        coordinates = goUp(higth, width)
-        wall[oldcolorpos] = red
-        pos = form2Dto1D(coordinates[0], coordinates[1])
-        higth = coordinates[0]
-        printLedWall(fromListToLedframe(wall))
-        
-        pass
-    
-    elif variable == '\x1b[B':
+def on_press(key):
+    global higth
+    global width
+    global oldcolorpos
+    try: variable = key.char # single-char keys
+    except: variable = key.name # other keys
+    if key == keyboard.Key.esc: return False # stop listener
+    if variable in ['down', 'up', 'left', 'right']: # keys interested
+        coordinates = [0,0]
+        if variable == "up":
+            print("up")
+            coordinates = goUp(higth, width)
+            wall[oldcolorpos] = red
+            pos = form2Dto1D(coordinates[0], coordinates[1])
+            higth = coordinates[0]
+            printLedWall(fromListToLedframe(wall))
+            
+            pass
+        elif variable == "down":
        
-        coordinates = goDown(higth,width)
-        wall[oldcolorpos] = red
-        pos = form2Dto1D(coordinates[0], coordinates[1])
-        higth = coordinates[0]
-        wall[pos] = blue
-        oldcolorpos = pos
-        printLedWall(fromListToLedframe(wall))
-        pass
-    
-    elif variable == '\x1b[D':
-       
-        coordinates = goLeft(higth, width)
-        wall[oldcolorpos] = red
-        pos = form2Dto1D(coordinates[0], coordinates[1])
-        width = coordinates[1]
-        wall[pos] = blue
-        oldcolorpos = pos
-        printLedWall(fromListToLedframe(wall))
-        print("left")
-        pass
-    
-    elif variable == '\x1b[C':
+            coordinates = goDown(higth,width)
+            wall[oldcolorpos] = red
+            pos = form2Dto1D(coordinates[0], coordinates[1])
+            higth = coordinates[0]
+            wall[pos] = blue
+            oldcolorpos = pos
+            printLedWall(fromListToLedframe(wall))
+            print("down")
+            pass
         
-        coordinates = goRight(higth, width)
-        wall[oldcolorpos] = red
-        pos = form2Dto1D(coordinates[0], coordinates[1])
-        width = coordinates[1]
-        wall[pos] = blue
-        oldcolorpos = pos
-        printLedWall(fromListToLedframe(wall))
-        print("right")
-        pass
-    
-    elif variable == "u":
-        print("byby BITCH")
-        flag = True
-    else:
-        print("are you dum ?")
-        pass
+        elif variable == "left":
+        
+            coordinates = goLeft(higth, width)
+            wall[oldcolorpos] = red
+            pos = form2Dto1D(coordinates[0], coordinates[1])
+            width = coordinates[1]
+            wall[pos] = blue
+            oldcolorpos = pos
+            printLedWall(fromListToLedframe(wall))
+            print("left")
+            pass
+        
+        elif variable == "right":
+            
+            coordinates = goRight(higth, width)
+            wall[oldcolorpos] = red
+            pos = form2Dto1D(coordinates[0], coordinates[1])
+            width = coordinates[1]
+            wall[pos] = blue
+            oldcolorpos = pos
+            printLedWall(fromListToLedframe(wall))
+            print("right")
+            pass
+        
+        elif variable == "u":
+            print("byby BITCH")
+            flag = True
+        else:
+            print("are you dum ?")
+            pass
+        #return False # remove this if want more keys
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# while j < 10:
-    
-#    
-#     oldcolorpos = randomVal
-#     randomVal = random.randint(1, 158)
-#     wall[oldcolorpos] = red
-#     wall[randomVal] = blue
-    
-#     for i in range(0, len(wall)):
-#         wall_format.append(wall[i][0])
-#         wall_format.append(wall[i][1])
-#         wall_format.append(wall[i][2])
-#     b_wall_format = bytearray(wall_format) 
-
-#     printLedWall(b_wall_format)
-#     time.sleep(1)
-#     j += 1
+lis = keyboard.Listener(on_press=on_press)
+lis.start() # start to listen on a separate thread
+lis.join() # no this if main thread is polling self.keys
